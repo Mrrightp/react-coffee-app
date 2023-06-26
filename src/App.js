2a4e6448af099865add1f055ee6845940171f2e8
Right possible <rightpossible1@gmail.com>
2023-06-26 01:36:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Types from "./pages/Types";
+import Benefits from "./pages/Benefits";
+import History from "./pages/History";
+import "./App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />/
+          <Route path="/types" element={<Types />} />
+          <Route path="/benefits" element={<Benefits />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
